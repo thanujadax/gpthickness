6ddefab55866419178e46c62932084df2a125364
Thanuja Ambegoda <thanujadax@gmail.com>
2015-10-13 17:54:35 +0200

    automated curve creation for all dist measures for all volumes

--- runAllCalibrationMethodsOnAllVolumes.m
+++ runAllCalibrationMethodsOnAllVolumes.m
@@ -4,13 +4,15 @@ function runAllCalibrationMethodsOnAllVolumes...
 % run all calibration methods for one volume and save the calibration
 % curves and the predictions in the outputPath
 
-distanceMeasure = 'COC';  % coefficient of correlation
+% distanceMeasure = 'COC';  % coefficient of correlation
 
-params.predict = 1; % set to 0 if only the interpolation curve is required.
+distanceMeasuresList = {'COC','SDI','MSE'};
+
+params.predict = 0; % set to 0 if only the interpolation curve is required.
 params.xyResolution = 5; % nm
-params.maxShift = 10;
+params.maxShift = 30;
 params.minShift = 0;
-params.maxNumImages = 700; % number of sections to initiate calibration.
+params.maxNumImages = 100; % number of sections to initiate calibration.
                 % the calibration curve is the mean value obtained by all
                 % these initiations
 params.numPairs = 1; % number of section pairs to be used to estimate the thickness of onesection
@@ -21,7 +23,7 @@ params.pathToPrecomputedCurve = '';
 params.imgStackFileExt = 'tif';
 
 imageCubeDirectory = '/home/thanuja/projects/data/FIBSEM_dataset/largercubes';
-outputSavePath = '/home/thanuja/projects/tests/thickness/similarityCurves/FIBSEM/20151001';
+outputSavePath = '/home/thanuja/projects/tests/thickness/similarityCurves/FIBSEM/20151013_allVols';
 
 diaryFile = fullfile(outputSavePath,'log.txt');
 diary(diaryFile);
@@ -34,40 +36,47 @@ diary(diaryFile);
 % get the tiff stack from each directory. some may have multiple tiff
 % stacks.
 
-for i=1:length(sampleDirectories)
-    
-    sampleSubDirName = sampleDirectories{i};
-    % read all image stacks in this sample
-    imageStackFileString = strcat('*.',params.imgStackFileExt);
-    imageStackFileString = fullfile(sampleSubDirName,imageStackFileString);
-    imageStackDir = dir(imageStackFileString);
-    
-%     str1 = sprintf('Processing image stack %s',sampleSubDirName,calibrationMethod);
-%     disp(str1)    
-    
-    % process each image stack in the sample
-    for j=1:length(imageStackDir)
-        inputImageStackFileName = fullfile...
-            (sampleSubDirName,imageStackDir(j).name);
-        
-        tokenizedFName = strsplit(inputImageStackFileName,filesep);
-        nameOfStack = strtok(tokenizedFName(end),'.');
-        nameOfStack = nameOfStack{1};
-        % check if subdir exists. if not create.
-        checkAndCreateSubDir(outputSavePath,nameOfStack);
-        outputSavePath_i = fullfile(outputSavePath,nameOfStack);
+for j=1:length(distanceMeasuresList)
+    % for all distance measures
+    distanceMeasure = distanceMeasuresList{j};
+    outputSaveDistMeasure = fullfile(outputSavePath,distanceMeasure);
+    checkAndCreateSubDir(outputSavePath,distanceMeasure)
+    for i=1:length(sampleDirectories)
 
-        % writes output to output path as txt file. Col vector.
-        for calibrationMethod=1:10
-            str1 = sprintf('Running calibration method %02d on image stack %s',calibrationMethod,sampleSubDirName);
-            disp(str1)
-            thicknessEstimates = doThicknessEstimation(...
-            calibrationMethod,inputImageStackFileName,outputSavePath_i,params,...
-            distanceMeasure);
+        sampleSubDirName = sampleDirectories{i};
+        % read all image stacks in this sample
+        imageStackFileString = strcat('*.',params.imgStackFileExt);
+        imageStackFileString = fullfile(sampleSubDirName,imageStackFileString);
+        imageStackDir = dir(imageStackFileString);
+
+    %     str1 = sprintf('Processing image stack %s',sampleSubDirName,calibrationMethod);
+    %     disp(str1)    
+
+        % process each image stack in the sample
+        for j=1:length(imageStackDir)
+            inputImageStackFileName = fullfile...
+                (sampleSubDirName,imageStackDir(j).name);
+
+            tokenizedFName = strsplit(inputImageStackFileName,filesep);
+            nameOfStack = strtok(tokenizedFName(end),'.');
+            nameOfStack = nameOfStack{1};
+            % check if subdir exists. if not create.
+            checkAndCreateSubDir(outputSaveDistMeasure,nameOfStack);
+            outputSavePath_i = fullfile(outputSaveDistMeasure,nameOfStack);
+
+            % writes output to output path as txt file. Col vector.
+            for calibrationMethod=1:9
+                str1 = sprintf...
+                    ('Running calibration method %02d on image stack %s using distMeasure %s',calibrationMethod,sampleSubDirName,distanceMeasure);
+                disp(str1)
+                thicknessEstimates = doThicknessEstimation(...
+                calibrationMethod,inputImageStackFileName,outputSavePath_i,params,...
+                distanceMeasure);
+            end    
         end    
-    end    
-end
+    end
 
+end
 % save parameters for future reference
 outputParamsFileName = fullfile(outputSavePath,'params.mat');
 save(outputParamsFileName,'params');
