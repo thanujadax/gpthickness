ff4378aa9ece2d8922c0b591fd74c0fde01be4b3
Thanuja Ambegoda <thanujadax@gmail.com>
2016-02-16 11:14:05 +0100

    added create data directory

---
+++ createData/removeFolds.m
@@ -0,0 +1,6 @@
+% create validation data using the sections with folds
+% read the sections
+% manually identify the bounding box containing the fold
+% replace this box in the sequence of images with average pix intensity of
+% the image
+% save the image sequence in the given location
