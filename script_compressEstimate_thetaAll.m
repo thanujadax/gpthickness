a03b530e6806515ebc4d71de3730ba9aea02de06
Thanuja Ambegoda <thanujadax@gmail.com>
2017-01-25 12:47:22 +0100

    init compress theta all

--- script_compressEstimate_thetaAll.m
+++ script_compressEstimate_thetaAll.m
@@ -2,11 +2,30 @@
 % rotated versions of the original image stack
 % rotations \theta = [0,10,90]
 
-% read image stack and rotate by t
-% extract cubic block with horizontal x axis (rotated axis)
+%% input file paths and parameters
+originalStackFileName = '/home/thanuja/DATA/ssSEM/20161215/tiff_blocks1/r2_c1_0_20_aligned2/r2_c1_0_20_aligned_2.tif';
+t = 30; % rotation in degrees
 
-% create and save gp models for new x and y axis
+%% read image stack and rotate by t
+im_original = double(imread(originalStackFileName));
+imtest = im_original(:,:,1);
+imtest = imtest./255;
+figure;imshow(imtest)
+% B = imrotate(A,angle,method,bbox) 
+% rotates image A, where bbox specifies the size of the returned image.
 
-% create inputs stacks of shifted images
+im_rotated = imrotate(imtest,t,'bilinear','crop');
+figure;imshow(im_rotated)
+cropSize = min(size(imtest))/sqrt(2);
+im_cropped = cropImageFromCenter(im_rotated,cropSize,cropSize);
+figure;imshow(im_cropped)
+%% extract cubic block with horizontal x axis (rotated axis)
 
-% run distance (thickness) estimator to predict \gamma_{yx}
+
+%% create and save gp models for new x and y axis
+
+
+%% create inputs stacks of shifted images
+
+
+%% run distance (thickness) estimator to predict \gamma_{yx}
